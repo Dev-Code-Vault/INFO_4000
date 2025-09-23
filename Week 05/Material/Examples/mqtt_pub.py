e25d44aff280c48e8287e82a5213bc869c89c0d3
Dev-Code-Vault <study.vault@proton.me>
2025-09-23 18:38:47 -0400

    week 5 start upload

---
+++ Week 05/Material/Examples/mqtt_pub.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:66c784a2a66069bba5f684d9fb893b7dd70e0d516fb30b2b13342809da6a86f4
+size 713
---
+++ Week 05/Material/Examples/paho_pub_sub.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0944575996b71144ab7109b7d06b2ca9cef0e0e2c7892cdaaa382b23f0d72dbd
+size 1186
